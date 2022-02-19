d3b05ffd4feee6f4dc2a0dc6ed8901cb93b6a40e
Alex Baena <alexbyd94@gmail.com>
2022-02-19 12:54:33 -0500

    cambio cssreset a cssNormalice

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 
     function mouseEnter() {
-       let a = document.getElementById("a").style.textShadow  = "0 0 30px #fff";
-        let link = document.getElementById("link").style.textShadow  = "0 0 30px #fff";
+        document.getElementById("a").style.textShadow  = "0 0 30px #fff";
+        document.getElementById("link").style.textShadow  = "0 0 30px #fff";
 }
 
     function mouseLeave() {
-        let a = document.getElementById("a").style.textShadow = "none";
-        let link = document.getElementById("link").style.textShadow = "none";
-
+    document.getElementById("a").style.textShadow  = "none";
+    document.getElementById("link").style.textShadow = "none";
 }
