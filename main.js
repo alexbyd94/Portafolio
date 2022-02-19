b0be5396223ebf23498cbe978e9d207da20e86eb
Alex Baena <alexbyd94@gmail.com>
2022-02-19 12:01:19 -0500

    agregamos mainjs y las demas hojas de estilos y tex-shadow

---
+++ main.js
@@ -0,0 +1,11 @@
+
+    function mouseEnter() {
+       let a = document.getElementById("a").style.textShadow  = "0 0 30px #fff";
+        let link = document.getElementById("link").style.textShadow  = "0 0 30px #fff";
+}
+
+    function mouseLeave() {
+        let a = document.getElementById("a").style.textShadow = "none";
+        let link = document.getElementById("link").style.textShadow = "none";
+
+}
